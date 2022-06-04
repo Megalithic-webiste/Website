fe63e503a56c6393153a0944b9c0547b8a30fe6b
Louis Cutner <lcutner@me.com>
2022-06-04 17:46:33 +0100

    search and functional sidebars

--- src/index.js
+++ src/index.js
@@ -6,6 +6,58 @@ const sidebar_left = document.getElementById("sidebar-left");
 const sidebar_right = document.getElementById("sidebar-right");
 const body = document.getElementById("body");
 const close_menu = document.getElementById("close-menu");
+const search_bar = document.getElementById("search-bar");
+const header_bar = document.getElementById("header-bar");
+const search_button = document.getElementById("search-button");
+const close_search = document.getElementById("close-search");
+const down_arrow_menu = document.getElementsByClassName("down-arrow-menu");
+const up_arrow_menu = document.getElementsByClassName("up-arrow-menu");
+const menu_content = document.getElementsByClassName("menu-content");
+
+console.log(up_arrow_menu);
+
+for (let i = 0; i < menu_content.length; i++) {
+  down_arrow_menu[i].addEventListener("click", function () {
+    for (let j = 0; j < menu_content.length; j++) {
+      if (up_arrow_menu[j].classList.contains("flex") === true) {
+        up_arrow_menu[j].classList.remove("flex");
+        up_arrow_menu[j].classList.add("hidden");
+        down_arrow_menu[j].classList.remove("hidden");
+        down_arrow_menu[j].classList.add("flex");
+        menu_content[j].classList.remove("flex");
+        menu_content[j].classList.add("hidden");
+      }
+      up_arrow_menu[i].classList.add("flex");
+      up_arrow_menu[i].classList.remove("hidden");
+      down_arrow_menu[i].classList.add("hidden");
+      down_arrow_menu[i].classList.remove("flex");
+      menu_content[i].classList.add("flex");
+      menu_content[i].classList.remove("hidden");
+    }
+  });
+  up_arrow_menu[i].addEventListener("click", function () {
+    up_arrow_menu[i].classList.remove("flex");
+    up_arrow_menu[i].classList.add("hidden");
+    down_arrow_menu[i].classList.remove("hidden");
+    down_arrow_menu[i].classList.add("flex");
+    menu_content[i].classList.remove("flex");
+    menu_content[i].classList.add("hidden");
+  });
+}
+
+search_button.addEventListener("click", function () {
+  search_bar.classList.remove("hidden");
+  search_bar.classList.add("flex");
+  header_bar.classList.add("hidden");
+  header_bar.classList.remove("flex");
+});
+
+close_search.addEventListener("click", function () {
+  search_bar.classList.add("hidden");
+  search_bar.classList.remove("flex");
+  header_bar.classList.remove("hidden");
+  header_bar.classList.add("flex");
+});
 
 const mediaQuery = window.matchMedia("(min-width: 1024px)");
 
